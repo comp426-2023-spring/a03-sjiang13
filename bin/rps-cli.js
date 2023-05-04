6872e0fd91510ce3fa74b06e9cdcf5cf14b54acf
Sophie Jiang <sjiang13@email.unc.edu>
2023-05-04 14:35:45 -0400

    first

---
+++ bin/rps-cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import { rps } from "/lib/rpsls.js"
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    console.log(`Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    process.exit(0);
+}
+
+if (args.r || args.rules) {
+    console.log(`Rules for Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`);
+    process.exit(0);
+
+}
+
+let shot = args._[0];
+try {
+    let result = rps(shot);
+    console.log(JSON.stringify(result));
+} catch (e) { 
+    console.log(help);
+    console.log(rules);
+}
