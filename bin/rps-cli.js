33dc71b7f75210766c514f99e0c0ef3d74477d30
Sophie Jiang <sjiang13@email.unc.edu>
2023-05-04 14:50:15 -0400

    help messages

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -36,6 +36,20 @@ try {
     let result = rps(shot);
     console.log(JSON.stringify(result));
 } catch (e) { 
-    console.log(help);
-    console.log(rules);
+    console.log(`Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    console.log(`Rules for Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`);
 }
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -39,7 +39,28 @@ try {
     let result = rpsls(shot);
     console.log(JSON.stringify(result));
 } catch (e) {
-    console.log(help);
-    console.log(rules);
+    console.log(`Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+    console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock SMOOSHES Lizard
+    - Lizard POISONS Spock
+    - Spock SMASHES Scissors
+    - Scissors DECAPITATES Lizard
+    - Lizard EATS Paper
+    - Paper DISPROVES Spock
+    - Spock VAPORIZES Rock
+    - Rock CRUSHES Scissors`);
 }
 
