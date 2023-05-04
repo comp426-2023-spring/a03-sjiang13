d9d51d0a3e7296c37726c40487b3ad359bfebca3
Sophie Jiang <sjiang13@email.unc.edu>
2023-05-04 14:43:56 -0400

    import

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
+
 import minimist from 'minimist';
-import { rps } from '../lib/rpsls.js';
+import { rps } from './lib/rpsls.js';
 
 
 const args = minimist(process.argv.slice(2));
